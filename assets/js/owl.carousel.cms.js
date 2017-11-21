cc8dad3e8bf4fef3025b6b9e0c642b9e81a5878f
CMS <support@joomlaman.com>
2017-11-21 14:02:51 +0700

    ok

--- assets/js/owl.carousel.cms.js
+++ assets/js/owl.carousel.cms.js
@@ -1,27 +1,34 @@
-(function($){
-	"use strict";
-    $(document).ready(function(){
-    	$(".cms-carousel").each(function(){
-    		var $this = $(this),slide_id = $this.attr('id'),slider_settings = cmscarousel[slide_id];
-            if($this.attr('data-slidersettings')){
-                slider_settings = jQuery.parseJSON($this.attr('data-slidersettings'));
-            }
-            else{
-                slider_settings.margin = parseInt(slider_settings.margin);
-                slider_settings.loop = (slider_settings.loop==="true");
-                slider_settings.mouseDrag = (slider_settings.mouseDrag==="true");
-                slider_settings.nav = (slider_settings.nav==="true");
-                slider_settings.dots = (slider_settings.dots==="true");
-                slider_settings.autoplay = (slider_settings.autoplay==="true");
-                slider_settings.autoplayTimeout =  parseInt(slider_settings.autoplayTimeout);
-                slider_settings.autoplayHoverPause = (slider_settings.autoplayHoverPause==="true");
-                slider_settings.smartSpeed = parseInt(slider_settings.smartSpeed);
-                if($('.cms-dot-container'+slide_id).length){
-                    slider_settings.dotsContainer = '.cms-dot-container'+slide_id;
-                    slider_settings.dotsEach = true;
+$(document).ready(function(){
+    /* =================
+     Carousel
+     =================== */
+    $(".cms-carousel").each(function() {
+
+        // VC 4.4 adds an empty div .vc_row-full-width somehow, get rid of them
+        $(this).find('> .vc_row-full-width').remove();
+
+        $(this).owlCarousel({
+            margin: parseInt($(this).attr('data-margin')),
+            loop: $(this).attr('data-loop') === 'true' ? true : false,
+            nav: $(this).attr('data-nav') === 'true' ? true : false,
+            mouseDrag: $(this).attr('data-mousedrag') === 'true' ? true : false,
+            navText:['<i class="fa fa-arrow-left"></i>','<i class="fa fa-arrow-right"></i>'],
+            dots: $(this).attr('data-dots') === 'true' ? true : false,
+            autoplay : $(this).attr('data-autoplay') === 'false' ? false : $(this).attr('data-autoplay'),
+            responsive:{
+                0:{
+                    items:parseInt($(this).attr('data-xsmall-items'))
+                },
+                768:{
+                    items:parseInt($(this).attr('data-small-items'))
+                },
+                992:{
+                    items:parseInt($(this).attr('data-medium-items'))
+                },
+                1200:{
+                    items:parseInt($(this).attr('data-large-items'))
                 }
             }
-    		$this.owlCarousel(slider_settings);
-    	});
+        });
     });
-})(jQuery)
+});
